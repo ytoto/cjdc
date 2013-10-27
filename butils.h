e43e2bd08ec5b04b233aba24c89ecf01504f2a5b
ytoto270 <ytoto270@hotmail.com>
2013-10-28 00:47:45 +0100

    moving bfuncs to butils.c

---
+++ butils.h
@@ -0,0 +1,6 @@
+/* benc utils */
+extern char *bskip(char *p);
+extern char *bvali(unsigned char i, char **p);
+extern char *bvals(unsigned char i, char **p);
+extern void bputi(const char *p);
+extern void bputs(const char *s);
--- peers.c
+++ peers.c
@@ -1,183 +1,7 @@
-#include <string.h>
 #include <unistd.h>
 
+#include "butils.h"
 #include "cjdc.h"
-
-static char *bskip(char *p)
-{
-	unsigned int u;
-	char c = *p;
-
-	switch (c) {
-	case 'd': /* skip dict */
-	case 'l': /* skip list */
-		++p;
-
-		do {
-			if ((p = bskip(p)) == NULL)
-				goto err;
-		} while (*p != 'e');
-
-		++p;
-		break;
-	case 'i': /* skip integer */
-		while (*p++ != 'e'); 
-		break;
-	default:
-		if (c >= '0' && c <= '9') { /* skip string */
-			p = a2u(p, &u);
-			++p;
-			p += u;
-		} else {
-err:			write(1, "error\n", 6);//TODO drop msg
-			p = NULL;
-		}
-	}
-
-	return p;
-}
-
-static char *bvali(unsigned char i, char **p)
-{
-	char *q = *p, *s = NULL;
-
-	if (*++q != ':')
-		goto err;
-
-	++q;
-	q += i;
-
-	if (*q != 'i') {
-err:		q = NULL;
-		goto out;
-	}
-
-	for (s = ++q; *q != 'e'; ++q);
-	++q;
-out:
-	*p = q;
-	return s;
-}
-
-static char *bvals(unsigned char i, char **p)
-{
-	char *q = *p, *s = NULL;
-	unsigned int u;
-
-	if (*++q != ':')
-		goto err;
-
-	++q;
-	q += i;
-	s = q;
-
-	if (*q >= '0' || *q <= '9') {
-		q = a2u(q, &u);
-		++q;
-		q += u;
-	} else
-err:		q = NULL;
-
-	*p = q;
-	return s;
-}
-
-static inline void bputi_(const char *p)
-{
-	const char *s = p;
-
-	for (; *p != 'e'; ++p);
-	write(1, s, p - s);
-}
-
-void bputi(const char *p)
-{
-	unsigned long long m, x, y, z;
-	char buf[5] = "  ", *s = buf;
-	char unit[] = " KMGTPEZ";
-	const char *q = p;
-	int i, n = 0, v;
-
-	if (p == NULL) {
-		s += 2;
-		*s++ = '-';
-		*s++ = '0';
-		goto out;
-	}
-
-	for (x = 0; *p != 'e'; ++p) {
-		x *= 10;
-		x += *p - '0';
-	}
-
-	v = p - q;
-
-	if (v <= 3) {
-		s += 3 - v;
-		s = u2a(s, x);
-		goto out;
-	}
-
-	y = x;
-
-	do {
-		v = y;
-		y >>= 10;
-
-		if (y > 0)
-			++n;
-	} while (y);
-
-	if (v < 10) {
-		for (i = 0, m = 1; i < n; ++i)
-			m <<= 10;
-
-		z = x - v * m;
-		*s++ = '0' + v;
-		*s++ = '.';
-
-		do {
-			v = z;
-			z >>= 10;
-		} while (z);
-
-		if (v > 999) /* 1000..1023 */
-			v = 0;
-
-		*s++ = '0' + (v / 100);
-	} else {
-		if (v > 999) /* 1000..1023 */
-			v = 999;
-			
-		if (v >= 100) {
-			*s++ = '0' + (v / 100);
-			v %= 100;
-		} else
-			*s++ = ' ';
-
-		*s++ = '0' + (v / 10);
-		*s++ = '0' + (v % 10);
-	}
-
-out:
-	*s++ = unit[n];
-	*s = ' ';
-	write(1, buf, 5);
-}
-
-static void bputs(const char *s)
-{
-	unsigned int u;
-
-	if (s) {
-		s = a2u((char *)s, &u);
-		++s;
-		write(1, s, u);
-	} else
-		write(1, "?", 1);
-}
-
-
 
 int peers(char *page, unsigned int size)
 {
@@ -208,11 +32,8 @@ int peers(char *page, unsigned int size)
 
 				while (*pk++ != ':');
 
-				if (pubk2ip6(pk, ip6) < 0) {
-					memset(ip6, ' ', 39);
-					ip6[0] = '?';
+				if (pubk2ip6(pk, ip6) < 0)
 					goto err;
-				}
 
 				ip6[39] = ' ';
 				write(1, ip6, 40);
@@ -315,7 +136,7 @@ skip2:					--p;
 			case 'e': /* end of top dict */
 				state &= ~inpeers;
 				++p;
-				goto out;
+				goto out_;
 			default:
 				goto skip;
 			}
@@ -338,9 +159,9 @@ skip:				p = bskip(p);
 
 		if (p == NULL) {
 err:			more = -1;
-			goto out;
+			break;
 		}
 	}
-out:
+out_:
 	return more;
 }
